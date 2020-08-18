63115132719abd5407c60d73830a69b22f8b8514
SkyNet322 <you@example.com>
2020-08-18 21:57:27 +0300

    deleted getFibonacciNumbers function

--- tasks/fibonacci.go
+++ tasks/fibonacci.go
@@ -1,19 +1,10 @@
 package tasks
 
-import "fmt"
-
 func fibonacci() func() int {
 	x := 0
 	y := 1
 	return func() int {
 		x, y = y, x+y
 		return x
-	}
-}
-
-func getFibonacciNumbers() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
 	}
 }
